6ed15d2d437c538b98a8053a1b814be4db80bc2e
deepwaterooo <heyan.huang2020@gmail.com>
2022-12-24 11:18:34 -0800

    LOVE MY DEAR COUSIN. .jar to details. LOVE MY DEAR COUSIN - MUST MARRY HIM AS SOON AS POSSIBLE

--- dwsdk/src/main/java/com/deepwaterooo/DWSDK.java
+++ dwsdk/src/main/java/com/deepwaterooo/DWSDK.java
@@ -1,99 +1,188 @@
 package com.deepwaterooo;
 
 import android.app.Activity;
-import android.app.NotificationManager;
-import android.content.Context;
-import android.content.Intent;
 import android.media.AudioManager;
-import android.os.Build;
-import android.os.Build.VERSION_CODES;
-import android.os.Bundle;
-import android.provider.Settings;
 import android.util.Log;
-import android.view.View;
 
-import com.deepwaterooo.dwsdk.R;
 import com.deepwaterooo.dwsdk.UnityasrEventCallback;
-import com.deepwaterooo.dwsdk.beans.ParentInfoDO;
-import com.deepwaterooo.dwsdk.utils.PlayerUtil;
+import com.deepwaterooo.sdk.beans.ParentInfoDO;
+import com.deepwaterooo.sdk.beans.PlayerDO;
+import com.deepwaterooo.sdk.networklayer.NetworkUtil;
+import com.deepwaterooo.sdk.utils.ApiCallListener;
+import com.deepwaterooo.sdk.utils.PlayerUtil;
+import com.deepwaterooo.sdk.utils.Util;
 import com.unity3d.player.UnityPlayer;
-import com.unity3d.player.UnityPlayerActivity;
 
-// 安卓SDK与unity游戏端最底层的双向桥接层:
+import java.io.IOException;
+import okhttp3.ResponseBody;
 
-// 那么这里桥接层两个类实现了双向调用,就可以达到互通目的了
-// 但是可以去测试,游戏端可以借助桥接方法调用这个类里所定义的方法,实现游戏调用安卓端
-// public class DWSDK extends AppCompatActivity { // 暂只设一个按钮,但是SDK这端的浓缩,这个按钮可转至游戏界面
-public class DWSDK { // 暂只设一个按钮,但是SDK这端的浓缩,这个按钮可转至游戏界面
-    private static final String TAG = "MainActivity";
+// Referenced classes of package com.deepwaterooo: DWUnityActivity
+public class DWSDK {
+    public static final String TAG = "DeepwateroooSDK"; // spu
+    public static final String UnityGameobjectName = "Deepwaterooo";
+    public static final int REQUEST_CODE_MANAGE_PLAYER = 0;
+    public static final int REQUEST_CODE_SELECT_PLAYER = 1;
 
-// 自身的引用: 会跳至安卓SDK的一端,就不需要下面的这个索引了
-    public static Activity mActivity;
-
-    private static UnityasrEventCallback mCallback;
-    private static AudioManager mAudioManager;
+    public DWSDK() {}
 
     public static void SendUnityMessage(String methodName, String parameter) {
-        Log.d(TAG, "SendUnityMessage() ");
-// 这里的控件对象: 应该是Deepwaterooo        
-        UnityPlayer.UnitySendMessage("Deepwaterooo", methodName, parameter != null ? parameter : ""); // for tmp
+        Log.d(TAG, "SendUnityMessage() methodName: " + methodName + ", parameter: " + parameter);
+        UnityPlayer.UnitySendMessage("Deepwaterooo", methodName, parameter != null ? parameter : "");
+    }
+    public static boolean IsChildSelected() {
+        Log.d(TAG, "IsChildSelected() ret (PlayerUtil.getSelectedPlayer(DWUnityActivity.instance) != null) " + (PlayerUtil.getSelectedPlayer(DWUnityActivity.instance) != null)); 
+        return PlayerUtil.getSelectedPlayer(DWUnityActivity.instance) != null;
     }
 
-// 暂时只增加这两三个公用接口,用于 调试 游戏端 与 安卓两个包裹的 封装 调用 流程    
-// <<<<<<<<<<<<<<<<<<<< 
+    public static void KeepAppAlive() {
+        Util.keepAppAlive();
+    }
+    public static boolean IsInternetConnected() {
+        return NetworkUtil.checkInternetConnection(DWUnityActivity.mUnityPlayer.getContext());
+    }
+    public static void UploadFileWithName(String data, String name) {
+        PlayerDO player = PlayerUtil.getSelectedPlayer(DWUnityActivity.instance);
+        if(player == null) {
+            return;
+        } else {
+            String n = (new StringBuilder()).append(player.getId()).append("_").append(name).append(".json").toString();
+            byte d[] = data.getBytes();
+            NetworkUtil.uploadFile(DWUnityActivity.instance, new ApiCallListener() {
+                    public void onResponse(Object o) { }
+                    public void onFailure(Object o) { }
+                }
+                , d, n);
+            return;
+        }
+    }
+    public static void DownloadFileWithName(String name) {
+        PlayerDO player = PlayerUtil.getSelectedPlayer(DWUnityActivity.instance);
+        if(player == null) {
+            return;
+        } else {
+            String n = (new StringBuilder()).append(player.getId()).append("_").append(name).append(".json").toString();
+            NetworkUtil.downloadFile(DWUnityActivity.instance, new ApiCallListener() {
+                    public void onResponse(Object o) {
+                        ResponseBody r = (ResponseBody)o;
+                        try {
+                            byte b[] = r.bytes();
+                            String retrieved = new String(b);
+                            DWSDK.SendUnityMessage("_onLoadFileSuccess", retrieved);
+                        } catch(IOException e) {
+                            e.printStackTrace();
+                            DWSDK.SendUnityMessage("_onLoadFileFail", "");
+                        }
+                    }
+                    public void onFailure(Object o) {
+                        DWSDK.SendUnityMessage("_onLoadFileFail", "");
+                    }
+                }
+                , n);
+            return;
+        }
+    }
     public static void Init() {
-        PlayerUtil.startSplashScreenActivity(DWUnityActivity.Instance);
+        PlayerUtil.startSplashScreenActivity(DWUnityActivity.instance);
     }
-// <<<<<<<<<<<<<<<<<<<<
     public static void StartSplashScreenActivity() {
-        PlayerUtil.startSplashScreenActivity(DWUnityActivity.Instance);
+        PlayerUtil.startSplashScreenActivity(DWUnityActivity.instance);
     }
-
     public static void StartGameActivity() {}
-//    public static void ManagePlayers() { // 这里都是说在游戏端UnityPlayer活动的上下文(不是上下文,是当前活动)中打开SDK界面
-//        PlayerUtil.startManagePlayerActivity(DWUnityActivity.Instance, 0);
-//    }
+
+    public static void ManagePlayers() { // 这里都是说在游戏端UnityPlayer活动的上下文(不是上下文,是当前活动)中打开SDK界面
+        PlayerUtil.startManagePlayerActivity(DWUnityActivity.instance, 0);
+    }
     public static boolean IsLoggedIn() {
-        ParentInfoDO info = PlayerUtil.getParentInfo(DWUnityActivity.Instance);
-        return info != null;
+//        ParentInfoDO info = PlayerUtil.getParentInfo(DWUnityActivity.instance);
+//        return info != null;
+        // 这里想要把这里跳过去,就直接返回 登录成功
+        return true;
     }
-//    public static void StartSelectPlayerActivity() {
-//        PlayerUtil.startSelectPlayerActivity(DWUnityActivity.Instance, false, 1);
-//    }
+    public static void StartSelectPlayerActivity() {
+        PlayerUtil.startSelectPlayerActivity(DWUnityActivity.instance, false, 1);
+    }
+    public static void GetProfileURL() {
+        String url = PlayerUtil.getSelectedPlayer(DWUnityActivity.instance).getProfileURL();
+        SendUnityMessage("profileURLResponse", url);
+    }
+    public static void Terms() {
+        PlayerUtil.showTermsNconditions(DWUnityActivity.instance);
+    }
+    public static void Privacy() {
+        PlayerUtil.showPrivacyPolicy(DWUnityActivity.instance);
+    }
+    public static void Credits() {
+        PlayerUtil.showCredits(DWUnityActivity.instance);
+    }
+    public static void StartPlaygroundActivity() {
+        PlayerUtil.startPlaygroundActivity(DWUnityActivity.instance);
+    }
+    public static void StartParentalCheckActivity() {
+        PlayerUtil.startParentalCheckActivity(DWUnityActivity.instance, 0);
+    }
+    public static void Logout() {
+        PlayerUtil.logoutUser(DWUnityActivity.instance);
+    }
+    public static String GetSelectedPlayer() {
+        return PlayerUtil.getSelectedPlayer(DWUnityActivity.instance).toString();
+    }
 
-    public static int add(int x, int y) { // 好像这个方法必须是静态的,那回去再试一下先前的项目
-        Log.d(TAG, "add() ");
-        String v = (x+y) + "";
-        DWSDK.SendUnityMessage("onAddResultReady", v);
-        return x+y;
-    }
+// // TODO: 就是下面的几个方法没太弄明白: 好像是java 7的什么写法,可以暂时不用管    
+//     public static void ShowAlertWarning(String title, String msg, String btnText, String methodName) { // 这个方法真的是没有用到
+//         Util.showAlertWarning(DWUnityActivity.mUnityPlayer.getContext(), title, msg, btnText,
+//                               new android.view.View.OnClickListener(methodName) {
+//                 final String val$methodName; 
+//                 public void onClick(View v) {
+//                     DWSDK.SendUnityMessage(methodName, "");
+//                 }
+// // 这里好像是我自己整的,当时没太搞明白是怎么回事                                  
+//                 {
+//                     //private OnClickListener init(String s) { // <<<<<<<<<<<<<<<<<<<< 应该是这行是不需要添加的
+//                      methodName = s;
+//                     super();
+//                 }
+//             }
+//             );
+//     }
+//     public static void ShowAlert(String title, String msg, String btnText1, String btnText2, String methodName1, String methodName2) {
+//         Util.showAlert(DWUnityActivity.mUnityPlayer.getContext(), title, msg, Text1, Text2, new android.view.View.OnClickListener(methodName1) {
+//                 final String val$methodName1;
+//                 public void onClick(View v) {
+//                     DWSDK.SendUnityMessage(methodName1, "");
+//                 }
+//                 {
+//                     super();
+//                     methodName1 = s;
+//                 }
+//             }, new android.view.View.OnClickListener(methodName2) {
+//                 final String val$methodName2;
+//                 public void onClick(View v) {
+//                     DWSDK.SendUnityMessage(methodName2, "");
+//                 }
+//                 {
+//                     methodName2 = s;
+//                     super();
+//                 }
+//             });
+//     }
     
-    // 获取接口内容
-    public static void setCallback(UnityasrEventCallback callback){
-        Log.d("@@@", "UnityBatteryEventCallback setCallback start ");
-        mCallback = callback;
-        Log.d("@@@", "UnityBatteryEventCallback setCallback end ");
-        mCallback.Test1("I love my dear cousin~! Must marry him");
-        mCallback.Speechcontent(617);
-    }
-
-// 上下文:都是需要上下文的,应该封装到小SDK中去
-//     public void click(View view){
-//         Log.d(TAG, "click() ");
-//         Intent intent = new Intent(this, DWUnityActivity.class);
-//         startActivity(intent);
+//     // 获取接口内容
+//     public static void setCallback(UnityasrEventCallback callback){
+//         Log.d("@@@", "UnityBatteryEventCallback setCallback start ");
+//         mCallback = callback;
+//         Log.d("@@@", "UnityBatteryEventCallback setCallback end ");
+//         mCallback.Test1("I love my dear cousin~! Must marry him");
+//         mCallback.Speechcontent(617);
 //     }
 //     public static int getMaxVolume() {
 //         Log.d(TAG, "getMaxVolume() (mAudioManager == null): " + (mAudioManager == null));
 //         int max = mAudioManager.getStreamMaxVolume( AudioManager.STREAM_MUSIC );
-//         Log.d(TAG, "getMaxVolume() max: " + max);
 //         mCallback.onIntValReady(max, "max");
 //         return max;
 //     }
 //     public static int getCurrentVolume() {
 //         Log.d(TAG, "getCurrentVolume() (mAudioManager == null): " + (mAudioManager == null));
 //         int current = mAudioManager.getStreamVolume( AudioManager.STREAM_MUSIC );
-//         Log.d(TAG, "getCurrentVolume() current: " + current);
 //         mCallback.onIntValReady(current, "cur");
 //         return current;
 //     }
@@ -107,15 +196,11 @@ public class DWSDK { // 暂只设一个按钮,但是SDK这端的浓缩,这个按
 //             startActivity(intent);
 //         }
 //         mAudioManager.setStreamVolume(AudioManager.STREAM_MUSIC, val, AudioManager.FLAG_PLAY_SOUND);
-// // TODO: 需要监听系统广播，需要得知是否设置成功了        
+// // TODO: 怎么知道是否设置成功了?
 //     }
-//    @Override // 作为独立的静态类,就不存在上下文,要用上下文要去别处拿
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-//         mAudioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
-//         Log.d(TAG, "onCreate() (mAudioManager == null): " + (mAudioManager == null));
-//        mActivity = this;
-//    }
-
+//     public void click(View view){
+//         Log.d(TAG, "click() ");
+//         Intent intent = new Intent(this, DWUnityActivity.class);
+//         startActivity(intent);
+//     }
 }
--- dwsdk/src/main/java/com/deepwaterooo/DWUnityActivity.java
+++ dwsdk/src/main/java/com/deepwaterooo/DWUnityActivity.java
@@ -7,18 +7,22 @@ import android.util.Log;
 import android.view.KeyEvent;
 import android.view.MotionEvent;
 import android.view.ViewGroup;
-import com.deepwaterooo.dwsdk.activities.BaseActivity;
-import com.deepwaterooo.dwsdk.utils.PlayerUtil;
+
+import com.deepwaterooo.sdk.activities.BaseActivity;
+import com.deepwaterooo.sdk.activities.DWBaseActivity;
+import com.deepwaterooo.sdk.beans.PlayerDO;
+import com.deepwaterooo.sdk.utils.PlayerUtil;
 import com.unity3d.player.UnityPlayer;
 
-// 尝试源项目的方法,把流程再测一遍
-public class DWUnityActivity extends BaseActivity {
-    private final String TAG = "DWUnityActivity";
+// Referenced classes of package com.deepwaterooo: DWSDK
+// 基类实现了LoginListener,这里就定义了这个接口类的两个回调方法
+public class DWUnityActivity extends DWBaseActivity { // 主要负责桥接:　安卓SDK向游戏端 传送信号通知
+    public static final String TAG = "DWUnityActivity";
 
-    private static BaseActivity _instance; // 安卓SDK 端
+    private static DWBaseActivity _instance;
 
-    public static UnityPlayer mUnityPlayer; // 游戏端
-    public static DWUnityActivity Instance;
+    public static UnityPlayer mUnityPlayer;
+    public static DWUnityActivity instance;
 
     private boolean _isScreenLocked;
     private boolean _fromBackground;
@@ -27,20 +31,18 @@ public class DWUnityActivity extends BaseActivity {
         _isScreenLocked = false;
         _fromBackground = false;
     }
-
     protected void onCreate(Bundle savedInstanceState) {
-        Log.d(TAG, "onCreate() ");
         _isScreenLocked = false;
-        this.requestWindowFeature(1);
+        requestWindowFeature(1);
         super.onCreate(savedInstanceState);
         if (mUnityPlayer == null) {
-            this.getWindow().setFormat(2);
+            getWindow().setFormat(2);
             mUnityPlayer = new UnityPlayer(this);
-            Instance = this;
+            instance = this;
         } else {
             ((ViewGroup)mUnityPlayer.getParent()).removeView(mUnityPlayer);
             UnityPlayer.currentActivity = this;
-            Instance = (DWUnityActivity)UnityPlayer.currentActivity;
+            instance = (DWUnityActivity)UnityPlayer.currentActivity;
         }
         setContentView(mUnityPlayer);
         mUnityPlayer.requestFocus();
@@ -56,8 +58,11 @@ public class DWUnityActivity extends BaseActivity {
         else if (resultCode == 1007)
             DWSDK.SendUnityMessage("UnlockPermissionResponse", "0");
     }
+    public void didClearTheCharctersOnBoard() {
+        DWSDK.SendUnityMessage("DidClearCharactersOnBoard", "");
+    }
 // 这里应该是从服务器触发得到的回调,它会进一步地回调给游戏端,传 1    
-    protected void onSuccessLogoutEvent() { // 这里这个回调是如何触发的:　BaseActivity基类
+    protected void onSuccessLogoutEvent() { // 这里这个回调是如何触发的:　BaseActivity基类,可以去找一下
         DWSDK.SendUnityMessage("SuccessLogout", "1");
     }
     protected void onPause() {
@@ -67,17 +72,15 @@ public class DWUnityActivity extends BaseActivity {
     protected void onResume() {
         super.onResume();
         mUnityPlayer.resume();
-// 这里只作测试: 让它开一个小SDK中的登录界面 好了
-        PlayerUtil.startSplashScreenActivity(Instance);
-        // if (!_isScreenLocked) {
-        //     if (_fromBackground) {
-        //         if (DWSDK.IsLoggedIn()) // 只里只作测试
-        //             // PlayerUtil.startSelectPlayerActivity(Instance, true, 1);
-        //         _fromBackground = false;
-        //     }
-        // } else {
-        //     _isScreenLocked = false;
-        // }
+        if(!_isScreenLocked) {
+            if(_fromBackground) {
+                if (DWSDK.IsLoggedIn())
+                    PlayerUtil.startSelectPlayerActivity(instance, true, 1);
+                _fromBackground = false;
+            }
+        } else {
+            _isScreenLocked = false;
+        }
     }
     public void gamePaused(boolean b) {
         DWSDK.SendUnityMessage("_onSDKScreenOpen", "");
@@ -98,7 +101,21 @@ public class DWUnityActivity extends BaseActivity {
         else
             return super.dispatchKeyEvent(event);
     }
-
+    protected void didNavigatesToMainMenu() {
+        DWSDK.SendUnityMessage("_onSDKScreenClose", "");
+    }
+// LoginListener: 2 个方法    
+    public void didFinishSdkUserConfiguration() {
+        DWSDK.SendUnityMessage("OnZPadFinishSDKUserConfig", "");
+    }
+    public void didSelectedChild(PlayerDO player) {
+        PlayerUtil.setSelectedPlayer(instance, player);
+        DWSDK.SendUnityMessage("_onProfileSelected", "");
+        DWSDK.SendUnityMessage("_onSDKScreenClose", "");
+    }
+    public void didfinishSDKscreenflow() {
+        DWSDK.SendUnityMessage("_onSDKReady", "");
+    }
     public boolean onKeyUp(int keyCode, KeyEvent event) {
         return mUnityPlayer.injectEvent(event);
     }
@@ -111,37 +128,12 @@ public class DWUnityActivity extends BaseActivity {
     public boolean onGenericMotionEvent(MotionEvent event) {
         return mUnityPlayer.injectEvent(event);
     }
-    public void batteryLevel(String s) {
-        DWSDK.SendUnityMessage("_onBatteryLevel", s);
-    }
-    public void availableServices() {
-    }
-    protected void didNavigatesToMainMenu() {
-        DWSDK.SendUnityMessage("_onSDKScreenClose", "");
-    }
-    public void didFinishSdkUserConfiguration() {
-        DWSDK.SendUnityMessage("OnZPadFinishSDKUserConfig", "");
-    }
-    public void didfinishSDKscreenflow() {
-        DWSDK.SendUnityMessage("_onSDKReady", "");
-    }
-
-    // public void didSelectedChild(PlayerDO player) {
-    //     PlayerUtil.setSelectedPlayer(Instance, player);
-    //     DWSDK.SendUnityMessage("_onProfileSelected", ""); // onUserLogin 强迫登录模式下才使用吧
-    //     DWSDK.SendUnityMessage("_onSDKScreenClose", "");
-    // }
 }
 
 // // 尝试源项目的方法,把流程再测一遍
 // public class DWUnityActivity extends BaseActivity { 
-// // public class DWUnityActivity extends UnityPlayerActivity { // 有个bug: 找不到DWSDK,换另一种方法试
-// // public class DWUnityActivity extends AppCompatActivity { // 感觉这里面有什么地方没有适配好,无法实例化启动程序
 //     private final String TAG = "DWUnityActivity";
-//     // public static UnityPlayer mUnityPlayer;
-//     // public static DWUnityActivity Instance; // 相当于是单例模式
-//     // // 自身的引用:
-//     // public static Activity mActivity;
+
 //     @Override
 //     protected void onCreate(Bundle bundle) {
 //         super.onCreate(bundle);
@@ -173,22 +165,4 @@ public class DWUnityActivity extends BaseActivity {
 // //         View unity_view = mUnityPlayer.getView(); // <<<<<<<<<<<<<<<<<<<< 感觉这里可能就是那个前不着村后不着店的死槛
 // //         ll_unity_container.addView(unity_view);
 //     }
-//     // public void click(View view){
-//     //     Log.d(TAG, "click() ");
-//     //     callMainActivity();
-//     // }
-//     // @Override
-//     // public boolean onKeyDown(int i, KeyEvent keyEvent) {
-//     //     // 添加返回键返回 MainActivity
-//     //     if (i == KeyEvent.KEYCODE_BACK){
-//     //         callMainActivity();
-//     //     }
-//     //     return super.onKeyDown(i, keyEvent);
-//     // }
-//     // private void callMainActivity(){
-//     //     Log.d(TAG, "callMainActivity() ");
-//     //     Intent intent = new Intent(this, com.deepwaterooo.DWSDK.class);
-//     //     startActivity(intent);
-//     //     finish();
-//     // }
 // }
